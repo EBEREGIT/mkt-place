50676cdbe2529b50a12a65959bae227cc6c74e08
EBEREGIT <samsoneberenjoku@gmail.com>
2020-07-13 23:43:11 +0100

    adjusted account page

--- src/components/Account/Login.js
+++ src/components/Account/Login.js
@@ -1,24 +1,27 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { Form, Button } from "react-bootstrap";
 
 export default function Login() {
   return (
-    <Form>
-      {/* phone number */}
-      <Form.Group controlId="formBasicEmail">
-        <Form.Label>Phone Number</Form.Label>
-        <Form.Control type="number" placeholder="Enter Phone Number" />
-      </Form.Group>
+    <Fragment>
+      <h3 className="text-center">Login</h3>
+      <Form>
+        {/* phone number */}
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Phone Number</Form.Label>
+          <Form.Control type="number" placeholder="Enter Phone Number" />
+        </Form.Group>
 
-      {/* password */}
-      <Form.Group controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder=" Enter Password" />
-      </Form.Group>
+        {/* password */}
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder=" Enter Password" />
+        </Form.Group>
 
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </Fragment>
   );
 }
--- src/components/Account/Register.js
+++ src/components/Account/Register.js
@@ -1,30 +1,33 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { Form, Button } from "react-bootstrap";
 
 export default function Register() {
   return (
-    <Form>
-      {/* phone number */}
-      <Form.Group controlId="formBasicEmail">
-        <Form.Label>Phone Number</Form.Label>
-        <Form.Control type="number" placeholder="Enter Phone Number" />
-      </Form.Group>
+    <Fragment>
+      <h3 className="text-center">Register</h3>
+      <Form>
+        {/* phone number */}
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Phone Number</Form.Label>
+          <Form.Control type="number" placeholder="Enter Phone Number" />
+        </Form.Group>
 
-      {/* password */}
-      <Form.Group controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder=" Enter Password" />
-      </Form.Group>
+        {/* password */}
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder=" Enter Password" />
+        </Form.Group>
 
-      {/* Shop Name */}
-      <Form.Group controlId="formBasicPassword">
-        <Form.Label>Shop Name</Form.Label>
-        <Form.Control type="text" placeholder="Enter Shop Name" />
-      </Form.Group>
+        {/* Shop Name */}
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Shop Name</Form.Label>
+          <Form.Control type="text" placeholder="Enter Shop Name" />
+        </Form.Group>
 
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </Fragment>
   );
 }
