c2adf7be480924b7bc5a664970c61642ad19c222
EBEREGIT <samsoneberenjoku@gmail.com>
2020-07-28 18:47:06 +0100

    made the offices page dynamic

--- src/components/Main/MainOffices.js
+++ src/components/Main/MainOffices.js
@@ -3,18 +3,10 @@ import React, { Fragment, useEffect, useState } from "react";
 import axios from "axios";
 
 // internal import
-import SingleOffice from './SingleOffice';
-
-// let offices = [];
-
-// for (let office = 0; office < 8; office++) {
-//     offices.push(
-//         <SingleOffice />
-//     )    
-// }
+import SingleOffice from "./SingleOffice";
 
 export default function MainOffices() {
-    // set all office state
+  // set all office state
   const [allOffices, setOffices] = useState([]);
 
   //   make API call
@@ -28,21 +20,12 @@ export default function MainOffices() {
       });
   }, []);
 
-//   //   collect each office and make them an array
-//   const offices = [];
-//   for (let eachOffice in allOffices.data) {
-//     offices.push(allOffices.data[eachOffice]);
-//   }
-
-console.log(allOffices.data)
-
-
-    return (
-        <Fragment>
+  return (
+    <Fragment>
       {allOffices.data &&
         allOffices.data.length > 0 &&
         allOffices.data.map((office) => (
-            <SingleOffice
+          <SingleOffice
             officeName={office.name}
             officeImage={office.photo}
             officeDescription={office.description}
@@ -50,5 +33,5 @@ console.log(allOffices.data)
           />
         ))}
     </Fragment>
-    )
+  );
 }
--- src/pages/Offices.js
+++ src/pages/Offices.js
@@ -1,21 +1,44 @@
-import React from "react";
-import { Row, Col, Container } from "react-bootstrap";
+// external imports
+import React, { useEffect, useState } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import axios from "axios";
+
+// internal import
 import SingleOffice from "../components/Main/SingleOffice";
 
-let offices = [];
-
-for (let office = 0; office < 40; office++) {
-  offices.push(<SingleOffice />);
-}
-
 export default function Offices() {
+  // set all office state
+  const [allOffices, setOffices] = useState([]);
+
+  //   make API call
+  useEffect(() => {
+    axios("https://afia.sjcmsportal.com/api/offices")
+      .then((result) => {
+        setOffices(result);
+      })
+      .catch((error) => {
+        error = Error;
+      });
+  }, []);
+
   return (
     <Container>
       <Row>
         <Col xs={12} sm={12} md={12} lg={12} className="headings">
           <h2>Offices</h2>
         </Col>
-        {offices}
+
+        {/* all offices */}
+        {allOffices.data &&
+        allOffices.data.length > 0 &&
+        allOffices.data.map((office) => (
+          <SingleOffice
+            officeName={office.name}
+            officeImage={office.photo}
+            officeDescription={office.description}
+            officeNumber={office.phone}
+          />
+        ))}
       </Row>
     </Container>
   );
